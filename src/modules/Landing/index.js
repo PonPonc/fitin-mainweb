f5bfc0c55afe5e6da296f9dc3f9b66b053de30c7
FranzGwapo <franzcasimir.ondiano@gmail.com>
2023-06-14 10:40:26 +0800

    Partial

--- src/modules/Landing/index.js
+++ src/modules/Landing/index.js
@@ -1,9 +1,44 @@
-import React, { useState } from 'react';
+import React from 'react';
+// import Header from 'modules/generic'
 
 const Landing = () =>{
  return(
-  <p>Hello</p>  
+    <div style={{width: '100%', height: '100vh', backgroundColor: '#39a8a2'}}>
+        <div style={{display: 'flex', width: '100%', height: '8vh', color: 'white'}}>
+            <div style={{width: '60%', alignItems: 'center',  alignItems: 'center'}}>
+            <p style={{marginTop: '2.5%', textIndent: '5%'}}>(Insert Logo)</p>
+            </div>
+            <div style={{width: '10%'}}>
+            <p style={{textAlign: 'center', marginTop: '15%'}}>Home</p>
+            </div>
+            <div style={{width: '10%'}}>
+            <p style={{textAlign: 'center', marginTop: '15%'}}>About</p>
+            </div>
+            <div style={{width: '10%'}}>
+            <p style={{textAlign: 'center', marginTop: '15%'}}>Services</p>
+            </div>
+            <div style={{width: '10%',  alignItems: 'center'}}>
+            <p style={{textAlign: 'center', marginTop: '15%'}}>Contact</p>
+            </div>
+        </div>
+        <hr style={{backgroundColor: '#2B7A78', border: 0 , width: '90%', height: '1%'}} />
+        <div style={{position: 'absolute',
+left: '664.49px',
+top: '136.28px',
+borderLeft: '20px solid transparent',
+    borderRight: '20px solid transparent',
+    borderBottom: 'calc(2 * 20px * 0.866) solid green',
+    borderTop: '20px solid transparent',
+    display: 'inline-block',
+transform: 'rotate(19.55deg)'}}>
+            
+        </div>
+    </div>
+    
+
+
+
  )
 }
 
-export default Landing
+export default Landing;
