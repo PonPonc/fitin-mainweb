71cd6a18d4f9098697410a7ee6313db92784a631
FranzGwapo <franzcasimir.ondiano@gmail.com>
2023-06-15 10:00:34 +0800

    [additional franz]

--- src/modules/generic/header.js
+++ src/modules/generic/header.js
@@ -2,23 +2,23 @@ import React from 'react';
 
 function Header() {
     return(
-      <div style={{display: 'flex', width: '100%', height: '10vh'}}>
-        <div style={{width: '60%',   alignItems: 'center',  alignItems: 'center'}}>
-          <p style={{marginTop: '2.5%'}}>Insert Logo</p>
+    <div style={{display: 'flex', width: '100%', height: '8vh', color: 'white', position: 'absolute'}}>
+        <div style={{width: '50%', alignItems: 'center',  alignItems: 'center'}}>
+        <p style={{marginTop: '2.5%', textIndent: '5%', fontSize: '1.3em', fontWeight: 'bold'}}>(Logo Here)</p>
         </div>
         <div style={{width: '10%'}}>
-          <p style={{textAlign: 'center', marginTop: '15%'}}>Home</p>
+        <p style={{textAlign: 'center', marginTop: '15%', fontSize: '1.3em', fontWeight: 'bold'}}>Home</p>
         </div>
         <div style={{width: '10%'}}>
-          <p style={{textAlign: 'center', marginTop: '15%'}}>About</p>
+        <p style={{textAlign: 'center', marginTop: '15%', fontSize: '1.3em', fontWeight: 'bold'}}>About</p>
         </div>
         <div style={{width: '10%'}}>
-          <p style={{textAlign: 'center', marginTop: '15%'}}>Services</p>
+        <p style={{textAlign: 'center', marginTop: '15%', fontSize: '1.3em', fontWeight: 'bold'}}>Services</p>
         </div>
         <div style={{width: '10%',  alignItems: 'center'}}>
-          <p style={{textAlign: 'center', marginTop: '15%'}}>Contact</p>
+        <p style={{textAlign: 'center', marginTop: '15%', fontSize: '1.3em', fontWeight: 'bold'}}>Contact</p>
         </div>
-      </div>
+    </div>
     );
 }
 
