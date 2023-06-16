0db5984561512a3db314fe77f582dd13a763bcb0
PonPonc <gybaez321@gmail.com>
2023-06-16 19:16:25 +0800

    Getting New Files

--- src/modules/generic/header.js
+++ src/modules/generic/header.js
@@ -3,11 +3,8 @@ import React from 'react';
 function Header() {
 
     return(
-    <div className='flex flex-col items-center text-tertiary'>
-        <div className='px-5 bg-dark w-[70%] text-center'>
-            <p>Available Soon in Google Play and App Store</p>
-        </div>
-        <div className='flex items-center px-10 py-6 border-b-[8px] w-[95%] border-accent'>
+    <div className='flex absolute flex-col items-center text-tertiary'>
+        <div className='flex items-center px-10 py-6 w-[95]'>
             <a>Logo Here</a>
             <div className='flex ml-auto gap-10'>
                 <a>Home</a>
